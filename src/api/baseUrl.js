8a044430708682176cf620eb0c4f014cf26034e4
bhavesh-kratos <bp3955@gmail.com>
2018-07-04 02:14:44 +0530

    first commit

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -4,7 +4,7 @@ const API_BASE_URL = 'https://api.shutterstock.com'; //LATER ONWARDS WE WILL MAK
 const instance = axios.create({
   baseURL: API_BASE_URL,
 });
-instance.defaults.headers.common.authorization = `Bearer 1/eyJjbGllbnRfaWQiOiIyMGFhMWMwYjM2ZGM4NmEyZjliMyIsInJlYWxtIjoiY3VzdG9tZXIiLCJzY29wZSI6InVzZXIudmlldyIsInV0diI6InpUak4iLCJ1c2VybmFtZSI6ImJwMzk1NTg3IiwidXNlcl9pZCI6MjAzNDE0OTY3LCJvcmdhbml6YXRpb25faWQiOm51bGwsIm9yZ2FuaXphdGlvbl91c2VyX2lkIjpudWxsLCJwYXJlbnRfb3JnYW5pemF0aW9uX2lkcyI6W10sImN1c3RvbWVyX2lkIjoyMDM0MTQ5NjcsImV4cCI6MTUzMDY1MjQ5M30.KY09PcNC7aC2b7wqvtPoOh4ALorCWxnHaSeQdseVSFuBjjwUUPpLUzGcywd5npqOVsLw0ghoqT9foIWyk6LGsg`;
+instance.defaults.headers.common.authorization = `Basic NTUwMjctMjNlOTQtODZiNmUtZWM4MzctMDgyOGUtMzUyMWQ6ZWVmNDEtY2Y1YjEtMzlkOGEtM2M0MmUtNjJkMGItNDQxOGE=`;
 
 export default instance;
 
